0a36d8a8a0e717f0fff6a83c5811ffb921462b69
moliva <oliva.miguelh@gmail.com>
2023-10-14 12:26:40 -0300

    refactor: key line component

--- src/components/ContentComponent.tsx
+++ src/components/ContentComponent.tsx
@@ -1,13 +1,15 @@
-import { For, createSignal } from 'solid-js'
+import { For } from 'solid-js'
 
 import { Content } from "../types"
-import { copyToClipboard } from '../utils'
+
+import { LineComponent } from './LineComponent'
 
 import styles from '../App.module.css'
 
 export type ContentProps = {
   initial?: boolean
   content: Content
+
   onCheckToggle(indices: number[]): void
 }
 
@@ -16,27 +18,10 @@ export const ContentComponent = (props: ContentProps) => {
 
   return <div class={initial ? styles['content-initial'] : styles.content}>
     <For each={content}>{
-      ([key, value], i) => {
-        let keyLabelRef: any
-
-        const [showMenu, setShowMenu] = createSignal(false)
-        const [blur, setBlur] = createSignal(!!key.blur)
-
-        const keyLine = key.line?.startsWith('http://') || key.line?.startsWith('https://')
-          ? (<a ref={keyLabelRef} href={key.line} target="_blank" class={`${styles.link} ${blur() ? styles.blur : ''}`}>{key.line}</a>)
-          : (<p ref={keyLabelRef} class={blur() ? styles.blur : ''}>{key.line}</p>)
-
-        return <>
-          <div class={styles['content-key']} onMouseEnter={() => setShowMenu(true)} onMouseLeave={() => setShowMenu(false)}>
-            {key.checkbox ? <input type="checkbox" checked={!!key.check} onClick={() => props.onCheckToggle([i()])} /> : null}
-            {keyLine}
-            {showMenu() ? <>
-              {key.blur ? <a class={styles['content-show-blur']} onClick={() => { setBlur(!blur()) }}>&#128065</a> : null}
-              <a class={styles['content-show-blur']} onClick={() => { copyToClipboard(key.line!) }}>&#x1f4cb</a>
-            </> : null}
-          </div>
-          <ContentComponent content={value} onCheckToggle={(indices) => props.onCheckToggle([i(), ...indices])} />
-        </>
-      }}</For>
+      ([key, value], i) => <>
+        <LineComponent key={key} onCheckToggle={() => props.onCheckToggle([i()])} />
+        <ContentComponent content={value} onCheckToggle={(indices) => props.onCheckToggle([i(), ...indices])} />
+      </>
+    }</For>
   </div>
 }
---
+++ src/components/LineComponent.tsx
@@ -0,0 +1,32 @@
+import { createSignal } from "solid-js"
+import { LineFormat } from "../types"
+
+import { copyToClipboard } from '../utils'
+
+import styles from '../App.module.css'
+
+export type LineProps = {
+  key: LineFormat
+
+  onCheckToggle(): void
+}
+
+export const LineComponent = (props: LineProps) => {
+  const { key, onCheckToggle } = props
+
+  const [showMenu, setShowMenu] = createSignal(false)
+  const [blur, setBlur] = createSignal(!!key.blur)
+
+  const keyLine = key.line?.startsWith('http://') || key.line?.startsWith('https://')
+    ? <a classList={{ [styles.blur]: blur() }} href={key.line} target="_blank" class={styles.link}>{key.line}</a>
+    : <p classList={{ [styles.blur]: blur() }}>{key.line}</p>
+
+  return <div class={styles['content-key']} onMouseEnter={() => setShowMenu(true)} onMouseLeave={() => setShowMenu(false)}>
+    {key.checkbox ? <input type="checkbox" checked={!!key.check} onClick={onCheckToggle} /> : null}
+    {keyLine}
+    {showMenu() ? <>
+      {key.blur ? <a class={styles['content-show-blur']} onClick={() => { setBlur(!blur()) }}>&#128065</a> : null}
+      <a class={styles['content-show-blur']} onClick={() => { copyToClipboard(key.line!) }}>&#x1f4cb</a>
+    </> : null}
+  </div>
+}
