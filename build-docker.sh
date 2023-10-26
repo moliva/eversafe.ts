ee408a7190861636a24a58518c6f6c84e818ef45
moliva <oliva.miguelh@gmail.com>
2023-10-25 23:22:51 -0300

    chore: bump docker

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dockerfile='Dockerfile'
-image_name='eversafe:v11'
+image_name='eversafe:v12'
 
 echo "Building $image_name"
 docker build  -f "$dockerfile" -t "$image_name" .
