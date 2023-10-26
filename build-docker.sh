14ca6c1ca78a4dd34c235cbfe6feb617ee1b44fc
moliva <oliva.miguelh@gmail.com>
2023-10-25 23:05:56 -0300

    chore:

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dockerfile='Dockerfile'
-image_name='eversafe:v10'
+image_name='eversafe:v11'
 
 echo "Building $image_name"
 docker build  -f "$dockerfile" -t "$image_name" .
