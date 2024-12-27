c886eb7282c7c9cbd1b1c2cd9ba6a98f12b49596
moliva <oliva.miguelh@gmail.com>
2024-12-27 14:16:10 -0300

    fix(login): redirect

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,9 @@
-import Fa from "solid-fa"
-import { faKey } from "@fortawesome/free-solid-svg-icons"
+import { useLocation } from '@solidjs/router'
 
-import { API_HOST } from "../services"
+import Fa from 'solid-fa'
+import { faKey } from '@fortawesome/free-solid-svg-icons'
+
+import { API_HOST } from '../services'
 
 import styles from '../App.module.css'
 import navStyles from './NavComponent.module.css'
@@ -9,9 +11,18 @@ import navStyles from './NavComponent.module.css'
 export type LoginProps = {}
 
 export function Login() {
-  return <div style={{ "min-height": "100vh", "align-items": "center", display: "flex", "justify-content": "center" }}>
-    <a href={`${API_HOST}/login`} class={`${styles.link} ${navStyles.login}`} style={{ "font-size": "30px", "font-weight": "bold" }}>
-      <Fa class={styles['nav-icon']} icon={faKey} /> Login
-    </a>
-  </div>
+  const location = useLocation()
+  const encoded = encodeURIComponent(location.pathname.replace('/tapiz', ''))
+
+  return (
+    <div style={{ 'min-height': '100vh', 'align-items': 'center', display: 'flex', 'justify-content': 'center' }}>
+      <a
+        href={`${API_HOST}/login?redirect=${encoded}`}
+        target='_self'
+        class={`${styles.link} ${navStyles.login}`}
+        style={{ 'font-size': '30px', 'font-weight': 'bold' }}>
+        <Fa class={styles['nav-icon']} icon={faKey} /> Login
+      </a>
+    </div>
+  )
 }
